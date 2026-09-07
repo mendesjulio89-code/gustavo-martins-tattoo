328a8b1af215abf54ef271706ad372c56691af6b
mendesjulio89-code <mendesjulio89@gmail.com>
2026-09-07 19:26:27 -0300

    Fix image path for blackwork closing design

--- config.js
+++ config.js
@@ -137,7 +137,7 @@ const PORTFOLIO_TRABALHOS = [
         titulo: "Adaga & Fechamento Blackwork",
         categoria: "blackwork",
         categoriaLabel: "BLACKWORK",
-        imagem: "images/fechamentos/fechamentos-2.jpeg"
+        imagem: "images/fechamentos/fechamentos2.jpeg"
     },
     {
         titulo: "Pescoço Free Hand Sombreado",
